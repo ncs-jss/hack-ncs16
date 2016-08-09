6c0ac8b8f344259bda044dafc7a3a6c3d609da93
devabhinav <abhinavpandey.1996@gmail.com>
2016-08-10 01:26:31 +0530

    Student  model added using sequelize

--- models/student.model.js
+++ models/student.model.js
@@ -1,1 +1,9 @@
 //models for student using sequelize
+module.exports = sequelize.define('student',{
+	id : {type : sequelize.INTEGER, autoIncrement : true, primaryKey : true},
+	email : { type : sequelize.STRING , allowNull : false},
+	name : { type : sequelize.STRING },
+	year : { type : sequelize.INTEGER,},
+	contactNo : { type : sequelize.STRING(10), allowNull : false },
+	admissionNo : { type : sequelize.STRING(9)}
+});
